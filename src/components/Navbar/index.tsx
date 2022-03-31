f5bc97917ce12fa73edafc6eec837744bbc9366c
OtavioMiranda23 <otaviomiranda@usp.br>
2022-03-31 16:30:27 -0300

    Alteração Navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,7 +2,7 @@ import { NavbarStyles, Title } from "./styles"
 import { MdMenu } from "react-icons/md"
 import { BiHelpCircle } from "react-icons/bi"
 import { FaCog } from "react-icons/fa"
-import { GrBarChart } from "react-icons/gr"
+import { BiBarChartAlt2 } from "react-icons/bi"
 
 export default function Navbar() {
   return (
@@ -13,7 +13,7 @@ export default function Navbar() {
       </div>
       <Title>Wordle</Title>
       <div>
-        <GrBarChart />
+        <BiBarChartAlt2 />
         <FaCog />
       </div>
     </NavbarStyles>
